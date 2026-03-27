4bb045e9ce6734036869644f88a9e272d3431e4d
Gowtham Sai Garnepudi <130050781+gsai9582@users.noreply.github.com>
2026-03-27 09:50:31 +0530

    Add files via upload

---
+++ Week-1/file (8).java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.function.Predicate;
+
+public class Task9_PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        Predicate<Integer> isPalindrome = n -> {
+            String s = String.valueOf(n);
+            return s.equals(new StringBuilder(s).reverse().toString());
+        };
+
+        System.out.println(isPalindrome.test(num) ? "Palindrome" : "Not Palindrome");
+        sc.close();
+    }
+}
---
+++ Week-1/file (9).java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Task10_SumOfLastDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = (a % 10) + (b % 10);
+        System.out.println(sum);
+        sc.close();
+    }
+}
